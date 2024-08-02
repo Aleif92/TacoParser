3175e436b00f4f3d5d25d9af698a857dfe027910
Alex Leifermann <aleif92@outlook.com>
2024-08-02 18:03:25 -0500

    Refactoring

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -12,13 +12,10 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
-            // Objective: Find the two Taco Bells that are the farthest apart from one another.
-            // Some of the TODO's are done for you to get you started. 
-
+            
             logger.LogInfo("Log initialized...");
 
-            // Use File.ReadAllLines(path) to grab all the lines from your csv file. 
-            // Optional: Log an error if you get 0 lines and a warning if you get 1 line
+           
             var lines = File.ReadAllLines(csvPath);
             if (lines.Length == 0)
             {
@@ -30,24 +27,18 @@ namespace LoggingKata
                 logger.LogWarning("File only has one line of input");
             }
 
-            // This will display the first item in your lines array
+            
             logger.LogInfo($"Lines: {lines[0]}");
 
-            // Create a new instance of your TacoParser class
+            
             var parser = new TacoParser();
-
-            // Use the Select LINQ method to parse every line in lines collection
             var locations = lines.Select(parser.Parse).ToArray();
 
   
-            // Complete the Parse method in TacoParser class first and then START BELOW ----------
-
-            // TODO: Create two `ITrackable` variables with initial values of `null`. 
-            // These will be used to store your two Taco Bells that are the farthest from each other.
             ITrackable tacoBell1 = null;
             ITrackable tacoBell2 = null;
 
-            // TODO: Create a `double` variable to store the distance
+            
             double distance = 0;
 
           
@@ -75,13 +66,7 @@ namespace LoggingKata
 
      
 
-            // TODO: Now, still being inside the scope of the second for loop, compare the two locations using `.GetDistanceTo()` method, which returns a double.
-            // If the distance is greater than the currently saved distance, update the distance variable and the two `ITrackable` variables you set above.
-
-            // NESTED LOOPS SECTION COMPLETE ---------------------
-
-            // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
-            // Display these two Taco Bell locations to the console.
+            
             logger.LogInfo($"{tacoBell1.Name} and {tacoBell2.Name} are the farthest apart.");
 
 
